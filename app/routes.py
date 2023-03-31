3ee6e656811199de5e1f408db728fecf494439be
diawando <abduldiawade777@gmail.com>
2023-03-31 21:53:38 +0000

    mise à jour de la route pour la collecte des données du login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -21,4 +21,9 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
