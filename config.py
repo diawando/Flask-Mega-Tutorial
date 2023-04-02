da82f0196d4eec16bb0012a1c1b341d828165d69
diawando <abduldiawade777@gmail.com>
2023-04-02 22:18:47 +0000

    ajout de psycopg et flask-migrate

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-gues'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \ 
-    'postgresql://postgres:lavie@localhost:5432/mega_tuto'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:lavie@localhost:5432/mega_tuto'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
