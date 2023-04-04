f4e319fd963257bde5201cd2375a30f9c6d8d0f6
diawando <abduldiawade777@gmail.com>
2023-04-04 22:08:27 +0000

    mis à jour de init.py, ajout du file_handler

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['abduldiawade777@gmail.com']
