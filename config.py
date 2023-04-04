6517649dbe8f363096c3245ce91f44990a133c8e
diawando <abduldiawade777@gmail.com>
2023-04-04 18:37:10 +0000

    mis à jour de config.py, configuration de server de messagerie

--- config.py
+++ config.py
@@ -4,4 +4,9 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-gues'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:lavie@localhost:5432/mega_tuto'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
