77c1e129c8487b8dca0c5e06abfc8376e0327d5e
diawando <abduldiawade777@gmail.com>
2023-04-07 01:58:48 +0000

    test et mis à jour

--- app/__init__.py
+++ app/__init__.py
@@ -39,7 +39,7 @@ if not app.debug:
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
                                         backupCount=10)
     file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(linenol)d]'
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
     ))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
