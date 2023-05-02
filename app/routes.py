e5671b5371879ef66a86f444f78f8ffcfa3746dd
diawando <abduldiawade777@gmail.com>
2023-05-02 16:00:31 +0000

    ajout des liens de pagination à la route index()

--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,13 @@ def index():
     posts = current_user.followed_posts().paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False
     ) 
-    return render_template('index.html', title='Home Page', form=form , posts=posts.items)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home Page', form=form , 
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -142,6 +148,6 @@ def unfollow(username):
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page=page, per_page=app.config['POST_PER_PAGE'], error_out=False
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False
     )
     return render_template('index.html', title='Explore', posts=posts.items)
